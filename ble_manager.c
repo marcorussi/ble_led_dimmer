ffa9286dc46596ca5240bcddb5e5afe3a89d3d0d
marcorussi <marco@marcorussi.net>
2016-03-26 08:37:11 +0000

    Changed LF clk source and removed upgrade zip file.

--- ble_manager.c
+++ ble_manager.c
@@ -54,10 +54,10 @@
 /* ----------------------- Local defines ------------------------ */ 
 
 /* Low frequency clock source to be used by the SoftDevice */
-#define NRF_CLOCK_LFCLKSRC      			{.source        = NRF_CLOCK_LF_SRC_XTAL,            \
-                                 		 	 .rc_ctiv       = 0,                                \
-                                 		 	 .rc_temp_ctiv  = 0,                                \
-                                 		 	 .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
+#define NRF_CLOCK_LFCLKSRC      			{.source        = NRF_CLOCK_LF_SRC_RC,				\
+                                 		 	 .rc_ctiv       = 15,                              	\
+                                 		 	 .rc_temp_ctiv  = 15,								\
+                                 		 	 .xtal_accuracy = 0}
 
 /* Number of central links used by the application. When changing this number remember to adjust the RAM settings */
 #define CENTRAL_LINK_COUNT              	0     
